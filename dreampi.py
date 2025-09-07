5d80571d72f4955bd8c55083e526d52f826e585a
zappepappe <zappepappe@users.noreply.github.com>
2025-09-07 22:09:25 +0200

    Update dreampi.py

--- dreampi.py
+++ dreampi.py
@@ -556,7 +556,7 @@ class Modem(object):
         )
 
     def disconnect(self):
-        if self._serial and self._serial.isOpen():
+        if self._serial and self._serial.is_open:
             self._serial.flush()
             self._serial.close()
             self._serial = None
