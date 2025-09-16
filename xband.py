5b52807a94f3d32918d5ee9ed03d88fc4cfa9d02
zappepappe <zappepappe@users.noreply.github.com>
2025-09-16 08:58:40 +0200

    Update xband.py

--- xband.py
+++ xband.py
@@ -70,7 +70,9 @@ def xbandInit():
     if os.path.exists(femtoSipPath) == False:
         try:
             os.makedirs(femtoSipPath)
-            r = requests.get("https://raw.githubusercontent.com/eaudunord/femtosip/master/femtosip.py")
+            r = requests.get(
+                "https://raw.githubusercontent.com/astoeckel/femtosip/8d8a3a2ba7534b4b805bb02dad826dc15ec8803b/femtosip.py"
+            )
             r.raise_for_status()
             with open(femtoSipPath+"/femtosip.py",'wb') as f:
                 text = r.content.decode('ascii','ignore').encode()
